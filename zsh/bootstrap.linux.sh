80f2ff9ed791646d8c5082c36d086c3f0e4a6db5
Jan Borsodi <jborsodi@gmail.com>
2016-03-17 18:11:25 +0100

    Fixed detection of curl package

--- zsh/bootstrap.linux.sh
+++ zsh/bootstrap.linux.sh
@@ -2,8 +2,10 @@
 set -e
 
 # Install some necessary packages for the rest of the system to work
-if [ -not $(dpkg -s curl 2&>1 1>/dev/null) ]; then
-  printf "  [ \033[00;34m..\033[0m ] Installing curl, needed for zsh installation"
+if [ ! dpkg -s curl 2>/dev/null 1>/dev/null ]; then
+  printf "  [ \033[00;34m..\033[0m ] Installing curl, needed for zsh installation\n"
   sudo apt-get install curl
+else
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] curl is installed\n"
 fi
 
