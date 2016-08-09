a15d6887f15237ebe329598a5404ff04dc9040e7
Jan Borsodi <jb@aplia.no>
2016-08-09 12:50:17 +0200

    Disable oh-my-zsh installation
    zplug is used for plugins to zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if ! [ -e $HOME/.oh-my-zsh/ ]; then
-    curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-fi
+#if ! [ -e $HOME/.oh-my-zsh/ ]; then
+#    curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+#fi
