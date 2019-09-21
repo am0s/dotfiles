05b72c8c6b76551bd15f9a80958ed05df2e754e8
Jan Borsodi <jborsodi@gmail.com>
2019-09-21 12:03:51 +0200

    script/install now also bootstraps
    Output of text has also improved.
    Added installer for zsh to install zfy.

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,3 +2,13 @@
 #if ! [ -e $HOME/.oh-my-zsh/ ]; then
 #    curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 #fi
+
+source $(dirname ..)/script/common
+
+if ! dpkg -l fzy >/dev/null; then
+    info "installing fzy"
+    sudo apt-get update -y && sudo apt-get install -y fzy
+    success "fzy installed"
+else
+    success "fzy installed"
+fi
