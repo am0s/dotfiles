d0f1fad0dc70bfb546756864d2bce58d33b44cbd
Jan Borsodi <jb@aplia.no>
2019-09-21 12:59:27 +0200

    Disabled fzy installation
    It is not available on all platforms and the attempt
    at shell interactive mode fails.
    Time to start on a Python implementation.

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,10 +5,31 @@
 
 source $(dirname ..)/script/common
 
-if ! dpkg -l fzy >/dev/null; then
-    info "fzy not found, installing\n"
-    sudo apt-get update -y && sudo apt-get install -y fzy
-    success "fzy installed"
-else
-    success "fzy installed"
-fi
+#if ! dpkg -l fzy >/dev/null 2>/dev/null; then
+#    install_it=
+#
+#    while true; do
+#      user "fzy not found: do you want to install it? [y]es, [n]o:"
+#      read -n 1 action
+#
+#      case "$action" in
+#        y|yes )
+#          install_it=true
+#          break
+#          ;;
+#        n|no )
+#          break
+#          ;;
+#        * )
+#          ;;
+#       esac
+#    done
+#
+#    if [ "$install_it" == "true" ]; then
+#        info "installing fzy\n"
+#        sudo apt-get update -y && sudo apt-get install -y fzy
+#        success "fzy installed"
+#    fi
+#else
+#    success "fzy installed"
+#fi
