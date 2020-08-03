cf03a9083ae61849bcca6055db08f32f1d9725d4
Jan Borsodi <jborsodi@gmail.com>
2020-08-03 07:50:37 +0000

    Change origin url and email after install
    If the dotfiles are installed using a https url then change it back
    to the SSH url to make it easier to develop on it.
    Also change email for repo to make sure all commits use my
    personal email.

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Swap to a ssh based url
+git remote set-url origin git@github.com:am0s/dotfiles.git
+
 # Check if dotfiles have been properly bootstrapped and installed
 if [[ ! -d $HOME/.dotfiles_root ]]; then
     echo "No dotfiles have been installed"
