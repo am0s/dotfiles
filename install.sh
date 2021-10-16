b12dca6bb4e56e4b6de4a04fbfca4412bea2649d
Jan <jborsodi@gmail.com>
2021-10-16 20:49:01 +0200

    Fixed git url swapping
    The git commands were not run in the dotfiles folder but rather
    wherever the user invoked the script.

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Swap to a ssh based url
-git remote set-url origin git@github.com:am0s/dotfiles.git
-
-# Force email for this repo
-git config --local user.email jborsodi@gmail.com
+(cd $DIR && \
+    # Swap to a ssh based url
+    git remote set-url origin git@github.com:am0s/dotfiles.git && \
+    # Force email for this repo
+    git config --local user.email jborsodi@gmail.com
+)
 
 # Check if dotfiles have been properly bootstrapped and installed
 if [[ ! -d $HOME/.dotfiles_root ]]; then
